0b32789f62eab23263e1b100d999c182de50b586
fred <domaine75@gmail.com>
2021-11-27 10:20:27 +0100

    check if DBeaver installation exists

--- DBeaverUpdateVersionCheck/Program.cs
+++ DBeaverUpdateVersionCheck/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -14,6 +15,34 @@ namespace DBeaverUpdateVersionCheck
       Display("Checking if there is a newer version of DBeaver, please wait ...");
       Display(string.Empty);
       // checking if DBeaver is installed in C:\Users\userName\AppData\Local\DBeaver
+      string userName = Environment.UserName;
+      string userNameProfile = Environment.GetFolderPath(Environment.SpecialFolder.UserProfile);
+      string appDatafolder = Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData);
+      // remove domain if any
+      if (userName.Contains("\\"))
+      {
+        userName = userName.Split('\\')[1];
+      }
+
+      string dbeaverInstallationPath = $"{appDatafolder}\\DBeaver";
+      string dbeaverReadMeFileName = "readme.txt";
+      bool dbeaverIsInstalled = false;
+      if (File.Exists(Path.Combine(dbeaverInstallationPath, dbeaverReadMeFileName)))
+      {
+        dbeaverIsInstalled = true;
+      }
+      else
+      {
+        dbeaverIsInstalled = false;
+      }
+
+      if (!dbeaverIsInstalled)
+      {
+        Display($"DBeaver has not been found in {Path.Combine(dbeaverInstallationPath, dbeaverReadMeFileName)}");
+        return;
+      }
+
+
 
       Display("Press any key to exit:");
       Console.ReadKey();
