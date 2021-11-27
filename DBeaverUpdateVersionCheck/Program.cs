598d45ede4363c9e65c57968c3f6c35d36ca8384
fred <domaine75@gmail.com>
2021-11-27 10:04:39 +0100

    Ajoutez des fichiers projet.

---
+++ DBeaverUpdateVersionCheck/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DBeaverUpdateVersionCheck
+{
+  internal class Program
+  {
+    static void Main()
+    {
+      Action<string> Display = Console.WriteLine;
+      Display("Checking if there is a newer version of DBeaver, please wait ...");
+      Display(string.Empty);
+      // checking if DBeaver is installed in C:\Users\userName\AppData\Local\DBeaver
+
+      Display("Press any key to exit:");
+      Console.ReadKey();
+    }
+  }
+}
